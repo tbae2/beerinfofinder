7ce1a5b0192f1af5df148b1f8b299a280114842a
tbae2 <thomas.bae0@gmail.com>
2016-11-08 16:09:38 -0500

    starting build out of api call helper

---
+++ app/utils/beerdbhelper.js
@@ -0,0 +1,20 @@
+var axios = require('axios');
+var breweryKey = require('../apikeys');
+
+//api key for brewerydb
+var key = breweryKey.breweryDb;
+
+
+
+function searchBeer(beerName){
+
+    return axios.get('http://api.brewerydb.com/v2/search?q=' + beerName + ' &type=beer&key=' + key + );
+
+};
+
+
+
+function getBeerInfo(query){
+
+
+};
